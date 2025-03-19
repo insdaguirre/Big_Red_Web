de9c1e075c334d8a6443cf6aac959ae7b8643245
Diego Aguirre <120332061+insdaguirre@users.noreply.github.com>
2025-03-19 12:05:42 -0400

    Initial commit of local project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactForm from './Pages/ContactForm';
+import LandnigPage from './Pages/LandnigPage';
+import Gallery from './Pages/Gallery';
+import Contactpage from './Pages/Contactpage';
+import Services from './Pages/Services';
+function App() {
+  return (
+    <div className="App">
+     <LandnigPage/>  
+      {/* <Gallery/>     */}
+      {/* <Contactpage/>   */}
+      {/* <ContactForm/>         */}
+     {/* <Services/> */}
+    </div>
+  );
+}
+
+export default App;
